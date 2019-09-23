fa01b4a1c03723b7bc2ba71da306257e9b2678fd
SebDet <sebastien.determe@intl.att.com>
2019-09-23 16:15:55 +0200

    Update onap.groovy

--- pipeline/onap.groovy
+++ pipeline/onap.groovy
@@ -4,7 +4,7 @@ node {
         echo "Building Component ${params.GERRIT_PROJECT}"
         //  build job: 'build-component', parameters: [string(name: 'GERRIT_CHANGE_NUMBER', value: env.GERRIT_CHANGE_NUMBER), string(name: 'GERRIT_PATCHSET_NUMBER', value: env.GERRIT_PATCHSET_NUMBER), string(name: 'GERRIT_REFSPEC', value: env.GERRIT_REFSPEC), string(name: 'ONAP_DOCKER_PREFIX', value: 'onap'), string(name: 'REGISTRY_DOCKER_PREFIX', value: 'new-onap'), string(name: 'PROJECT_REGISTRY', value: 'localhost:443'), string(name: 'PROJECT', value: env.GERRIT_PROJECT)]
         
-        sh("bash pwd")
+        sh("pwd")
         sh("bash scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
         def buildScript = load "build/${params.GERRIT_PROJECT}/build-component.groovy"
         buildScript.buildComponent()
