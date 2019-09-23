42877aad67227251ab011beccc6706ff7cec649d
SebDet <sebastien.determe@intl.att.com>
2019-09-23 17:22:48 +0200

    Update onap.groovy

--- pipeline/onap.groovy
+++ pipeline/onap.groovy
@@ -3,13 +3,13 @@ node {
    stage('Build New Docker') {
         echo "Building Component ${params.GERRIT_PROJECT}"
         //  build job: 'build-component', parameters: [string(name: 'GERRIT_CHANGE_NUMBER', value: env.GERRIT_CHANGE_NUMBER), string(name: 'GERRIT_PATCHSET_NUMBER', value: env.GERRIT_PATCHSET_NUMBER), string(name: 'GERRIT_REFSPEC', value: env.GERRIT_REFSPEC), string(name: 'ONAP_DOCKER_PREFIX', value: 'onap'), string(name: 'REGISTRY_DOCKER_PREFIX', value: 'new-onap'), string(name: 'PROJECT_REGISTRY', value: 'localhost:443'), string(name: 'PROJECT', value: env.GERRIT_PROJECT)]
-        
-        sh("ls -latr")
-        sh("ls -latr scripts")
-        sh("bash -x scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
+        git(url: 'git@github.com:sebdet/onap-azure-cicd.git',credentialsId: 'github-key-cicd-project', branch: "master")
+        git(url: 'OnapTesterBot@${params.GERRIT_HOST}:${params.GERRIT_PORT}/${params.GERRIT_PROJECT}',credentialsId: 'lf-key-onap-bot', branch: "FETCH_HEAD", refspec: "${params.GERRIT_REFSPEC}")
+      
+        sh("bash scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
         def buildScript = load "build/${params.GERRIT_PROJECT}/build-component.groovy"
         buildScript.buildComponent()
-        sh("bash -x scripts/docker/retag-images.sh -p $ONAP_DOCKER_PREFIX -n $REGISTRY_DOCKER_PREFIX -r $REGISTRY_HOST -v $GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER")
+        sh("bash scripts/docker/retag-images.sh -p $ONAP_DOCKER_PREFIX -n $REGISTRY_DOCKER_PREFIX -r $REGISTRY_HOST -v $GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER")
     }
     stage('Deploy New Docker For Component') {
         echo 'Skipping Deploy for now'
