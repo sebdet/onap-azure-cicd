1eada7de72ff961bafbde8cb6e1f841a811cb06e
SebDet <sebastien.determe@intl.att.com>
2019-09-23 14:22:27 +0200

    Create build-component.groovy

---
+++ pipeline/build/so/build-component.groovy
@@ -0,0 +1,13 @@
+def buildComponent() {
+    withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+        maven: 'maven-3',
+        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+        // We recommend to define Maven settings.xml globally at the folder level using 
+        // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
+        // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
+        mavenSettingsConfig: 'my-maven-settings') {
+      // Run the maven build
+      sh "mvn clean install -P docker -Dmaven.test.skip=true"
+    }
+}
