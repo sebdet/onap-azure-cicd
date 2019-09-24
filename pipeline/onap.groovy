56664a5e323d6e6ef4cbbd09b89d526395b06096
SebDet <sebastien.determe@intl.att.com>
2019-09-24 10:37:13 +0200

    Update onap.groovy

--- pipeline/onap.groovy
+++ pipeline/onap.groovy
@@ -24,7 +24,7 @@ node {
                                        name: 'onap_project']]])
       
         sh("bash -x onap-azure-cicd/scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
-        def buildScript = load "onap-azure-cicd/build/${params.GERRIT_PROJECT}/build-component.groovy"
+        def buildScript = load "onap-azure-cicd/pipeline/build/${params.GERRIT_PROJECT}/build-component.groovy"
         buildScript.buildComponent()
         sh("bash -x onap-azure-cicd/scripts/docker/retag-images.sh -p $ONAP_DOCKER_PREFIX -n $REGISTRY_DOCKER_PREFIX -r $REGISTRY_HOST -v $GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER")
     }
