77378ad2bea0442e01af947246a3558bc12b18a0
SebDet <sebastien.determe@intl.att.com>
2019-09-24 11:01:13 +0200

    Update build-component.groovy

--- pipeline/build/clamp/build-component.groovy
+++ pipeline/build/clamp/build-component.groovy
@@ -7,7 +7,7 @@ def buildComponent(componentPath='clamp') {
         // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
         // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
         mavenSettingsFilePath: '/var/lib/jenkins/maven-settings.xml'
-    ) {
+        mavenLocalRepo: '${WORKSPACE}/maven_repo') {
       // Run the maven build
         sh "mvn -f ${componentPath}/pom.xml --batch-mode clean install -P docker -Dmaven.test.skip=true"
     }
