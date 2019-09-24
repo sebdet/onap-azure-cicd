631623d93da6e104cabb346672f12c2b2a033878
SebDet <sebastien.determe@intl.att.com>
2019-09-24 15:11:34 +0200

    Update onap.groovy

--- pipeline/onap.groovy
+++ pipeline/onap.groovy
@@ -23,26 +23,24 @@ node {
                                        url: '${GERRIT_SCHEME}://OnapTesterBot@${GERRIT_HOST}:${GERRIT_PORT}/${GERRIT_PROJECT}', 
                                        name: 'onap_project']]])
       
-        sh("bash -x onap-azure-cicd/scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
-        def buildScript = load "onap-azure-cicd/pipeline/build/${params.GERRIT_PROJECT}/build-component.groovy"
-        buildScript.buildComponent(params.GERRIT_PROJECT)
-        sh("bash -x onap-azure-cicd/scripts/docker/tag-images.sh -p $ONAP_DOCKER_PREFIX -n $REGISTRY_DOCKER_PREFIX -r $REGISTRY_HOST -v $GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER")
+        //sh("bash -x onap-azure-cicd/scripts/docker/create-registry.sh -d $CERTIFICATE_FOLDER -c $CERTIFICATE_FILENAME -k $KEY_FILENAME")
+        //def buildScript = load "onap-azure-cicd/pipeline/build/${params.GERRIT_PROJECT}/build-component.groovy"
+        //buildScript.buildComponent(params.GERRIT_PROJECT)
+        //sh("bash -x onap-azure-cicd/scripts/docker/tag-images.sh -p $ONAP_DOCKER_PREFIX -n $REGISTRY_DOCKER_PREFIX -r $REGISTRY_HOST -v $GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER")
     }
     stage('Deploy New Docker For Component') {
         echo "Deploying Component ${params.GERRIT_PROJECT} on Azure OOM lab"
         //  build job: 'deploy-component', parameters: [string(name: 'OOM_FOLDER', value: 'oom'), string(name: 'COMPONENT_NAME', value: env.GERRIT_PROJECT), string(name: 'ONAP_DOCKER_PREFIX', value: 'onap'), string(name: 'REGISTRY_DOCKER_PREFIX', value: 'new-onap'), string(name: 'GERRIT_REVIEW', value: env.GERRIT_CHANGE_NUMBER), string(name: 'GERRIT_PATCHSET', value: env.GERRIT_PATCHSET_NUMBER), string(name: 'HELM_RELEASE_NAME', value: 'cc697w-tdpi'), string(name: 'REGISTRY_DOCKER', value: 'onapci.westus2.cloudapp.azure.com:443')]
-            checkout([$class: 'GitSCM', 
-                  branches: [[name: 'master']], 
-                  doGenerateSubmoduleConfigurations: false, 
-                  extensions: [[$class: 'RelativeTargetDirectory', 
-                                relativeTargetDir: '${OOM_FOLDER}']], 
-                  submoduleCfg: [], 
-                  userRemoteConfigs: [[credentialsId: 'lf-key-onap-bot', 
-                                       url: '${GERRIT_SCHEME}://OnapTesterBot@${GERRIT_HOST}:${GERRIT_PORT}/oom', 
-                                       name: 'onap_oom_project']]])
-            sh("cd $OOM_FOLDER/kubernetes/")
-            sh("pwd")
-            sh("make all")
+        checkout([$class: 'GitSCM', 
+            branches: [[name: 'master']], 
+            doGenerateSubmoduleConfigurations: false, 
+            extensions: [[$class: 'RelativeTargetDirectory', 
+                          relativeTargetDir: '${OOM_FOLDER}']], 
+            submoduleCfg: [], 
+            userRemoteConfigs: [[credentialsId: 'lf-key-onap-bot', 
+                          url: '${GERRIT_SCHEME}://OnapTesterBot@${GERRIT_HOST}:${GERRIT_PORT}/oom', 
+                          name: 'onap_oom_project']]])
+            sh("make -c $OOM_FOLDER/kubernetes/ all")
             sh("bash -x $WORKSPACE/scripts/create-image-template.sh -d $WORKSPACE/$OOM_FOLDER/kubernetes/$GERRIT_PROJECT -c $GERRIT_PROJECT -p $ONAP_DOCKER_PREFIX -r $REGISTRY_HOST -n $REGISTRY_DOCKER_PREFIX -v $GERRIT_REVIEW-$GERRIT_PATCHSET -o $WORKSPACE/$OOM_FOLDER/override-onap.yaml")
             sh("bash -x $WORKSPACE/scripts/upgrade-component.sh -d $WORKSPACE/$OOM_FOLDER/kubernetes -r $HELM_RELEASE_NAME-$GERRIT_PROJECT -c $GERRIT_PROJECT -f $WORKSPACE/$OOM_FOLDER/override-onap.yaml")
     }
