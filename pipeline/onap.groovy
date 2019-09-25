3492d08da534db233626e68b6936e53233c3fc6c
SebDet <sebastien.determe@intl.att.com>
2019-09-25 12:41:54 +0200

    Update onap.groovy

--- pipeline/onap.groovy
+++ pipeline/onap.groovy
@@ -49,9 +49,9 @@ node {
        if (UPGRADE_STATUS != 0) {
          // For SSH private key authentication, try the sshagent step from the SSH Agent plugin.
          sshagent (credentials: ['github-key-cicd-project']) {
-               sh("git --git-dir=${WORKSPACE}/onap-azure-cicd add ${WORKSPACE}/onap-azure-cicd/job-results/${GERRIT_PROJECT}/${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}/deployment/*")
-               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd commit -m \"Deployment of project: $GERRIT_PROJECT, review: $GERRIT_CHANGE_URL\"')
-               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd push origin master')
+               sh("git --git-dir=${WORKSPACE}/onap-azure-cicd/.git add ${WORKSPACE}/onap-azure-cicd/job-results/${GERRIT_PROJECT}/${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}/deployment/*")
+               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd/.git commit -m \"Deployment of project: $GERRIT_PROJECT, review: $GERRIT_CHANGE_URL\"')
+               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd/.git push origin master')
          }
          sshagent (credentials: ['lf-key-onap-bot']) {
                sh(script: "ssh -p $GERRIT_PORT OnapTesterBot@$GERRIT_HOST gerrit review --project $GERRIT_PROJECT --message \'\"WARNING: OOM deployment had issues, check the logs: https://github.com/sebdet/onap-azure-cicd/tree/master/job-results/$GERRIT_PROJECT/$GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER/deployment\"\' $GERRIT_PATCHSET_REVISION")
@@ -71,9 +71,9 @@ node {
             
             // For SSH private key authentication, try the sshagent step from the SSH Agent plugin.
             sshagent (credentials: ['github-key-cicd-project']) {
-               sh("git --git-dir=${WORKSPACE}/onap-azure-cicd add ${WORKSPACE}/onap-azure-cicd/job-results/${GERRIT_PROJECT}/${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}/tests/*")
-               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd commit -m \"Result of project: $GERRIT_PROJECT, review: $GERRIT_CHANGE_URL\"')
-                sh('git --git-dir=${WORKSPACE}/onap-azure-cicd push origin master')
+               sh("git --git-dir=${WORKSPACE}/onap-azure-cicd/.git add ${WORKSPACE}/onap-azure-cicd/job-results/${GERRIT_PROJECT}/${GERRIT_CHANGE_NUMBER}-${GERRIT_PATCHSET_NUMBER}/tests/*")
+               sh('git --git-dir=${WORKSPACE}/onap-azure-cicd/.git commit -m \"Result of project: $GERRIT_PROJECT, review: $GERRIT_CHANGE_URL\"')
+                sh('git --git-dir=${WORKSPACE}/onap-azure-cicd/.git push origin master')
             }
             sshagent (credentials: ['lf-key-onap-bot']) {
                 sh(script: "ssh -p $GERRIT_PORT OnapTesterBot@$GERRIT_HOST gerrit review --project $GERRIT_PROJECT --message \'\"ERROR: OOM tests have FAILED, check the results: https://github.com/sebdet/onap-azure-cicd/tree/master/job-results/$GERRIT_PROJECT/$GERRIT_CHANGE_NUMBER-$GERRIT_PATCHSET_NUMBER/tests\"\' $GERRIT_PATCHSET_REVISION")
